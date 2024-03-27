ba78a888af888067f853e7c09496fb4af7ddfb64
mengfly <7957086+mengfly_p@user.noreply.gitee.com>
2024-03-27 10:20:29 +0800

    feat: 图片添加padding属性

--- src/main/java/io/github/mengfly/excel/report/component/image/ImageComponent.java
+++ src/main/java/io/github/mengfly/excel/report/component/image/ImageComponent.java
@@ -1,6 +1,7 @@
 package io.github.mengfly.excel.report.component.image;
 
 import io.github.mengfly.excel.report.component.AbstractComponent;
+import io.github.mengfly.excel.report.entity.Border;
 import io.github.mengfly.excel.report.entity.Point;
 import io.github.mengfly.excel.report.entity.Size;
 import io.github.mengfly.excel.report.excel.ExcelCellSpan;
@@ -8,6 +9,7 @@ import io.github.mengfly.excel.report.excel.ReportContext;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.poi.ss.usermodel.ClientAnchor;
+import org.apache.poi.util.Units;
 import org.apache.poi.xssf.usermodel.XSSFDrawing;
 import org.apache.poi.xssf.usermodel.XSSFPicture;
 
@@ -21,6 +23,7 @@ public class ImageComponent extends AbstractComponent {
     private Image image;
     private ScaleType scaleType = ScaleType.FIT_XY;
     private ClientAnchor.AnchorType anchorType;
+    private String padding = "2,2,2,2";
 
     @Override
     public void onExport(ReportContext context, Point point) {
@@ -35,7 +38,22 @@ public class ImageComponent extends AbstractComponent {
                 final ClientAnchor fillAnchor = cellSpan.getFillAnchor(anchorType);
                 final XSSFPicture picture = patriarch.createPicture(fillAnchor, pictureIndex);
                 final Dimension imageDimension = picture.getImageDimension();
-                context.addOnExportFinalizer(() -> scaleType.onAnchor(fillAnchor, cellSpan, imageDimension));
+                context.addOnExportFinalizer(() -> {
+                    scaleType.onAnchor(fillAnchor, cellSpan, imageDimension);
+                    Border padding = Border.of(getPadding());
+
+                    int top = padding.getTop() * Units.EMU_PER_PIXEL;
+                    int bottom = -padding.getBottom() * Units.EMU_PER_PIXEL;
+
+                    int left = padding.getLeft() * Units.EMU_PER_POINT;
+                    int right = -padding.getRight() * Units.EMU_PER_POINT;
+
+                    fillAnchor.setDx1(fillAnchor.getDx1() + left);
+                    fillAnchor.setDx2(fillAnchor.getDx2() + right);
+
+                    fillAnchor.setDy1(fillAnchor.getDy1() + top);
+                    fillAnchor.setDy2(fillAnchor.getDy2() + bottom);
+                });
 
             } catch (Exception e) {
                 cellSpan.setValue(e.getMessage());
--- src/main/java/io/github/mengfly/excel/report/entity/Border.java
+++ src/main/java/io/github/mengfly/excel/report/entity/Border.java
@@ -1,5 +1,7 @@
 package io.github.mengfly.excel.report.entity;
 
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.util.StrUtil;
 import lombok.Builder;
 import lombok.Data;
 
@@ -13,5 +15,68 @@ public class Border {
     private final int left;
     private final int right;
     private final int bottom;
+
+    public static Border of(int top, int bottom, int left, int right) {
+        return Border.builder()
+                .top(top)
+                .left(left)
+                .right(right)
+                .bottom(bottom)
+                .build();
+    }
+
+    public static Border of(String str) {
+        if (StrUtil.isEmpty(str)) {
+            return EMPTY;
+        }
+        final String[] split = str.split(",");
+        if (split.length == 0) {
+            return EMPTY;
+        }
+        int top;
+        int bottom;
+        int left;
+        int right;
+
+        if (split.length == 1) {
+            final Integer n1 = Convert.toInt(split[0], 0);
+            top = n1;
+            bottom = n1;
+            left = n1;
+            right = n1;
+        } else if (split.length == 2) {
+            final Integer n1 = Convert.toInt(split[0], 0);
+            final Integer n2 = Convert.toInt(split[1], 0);
+            top = n1;
+            bottom = n1;
+            left = n2;
+            right = n2;
+        } else if (split.length == 3) {
+            final Integer n1 = Convert.toInt(split[0], 0);
+            final Integer n2 = Convert.toInt(split[1], 0);
+            final Integer n3 = Convert.toInt(split[2], 0);
+            top = n1;
+            bottom = n2;
+            left = n3;
+            right = n3;
+        } else {
+            final Integer n1 = Convert.toInt(split[0], 0);
+            final Integer n2 = Convert.toInt(split[1], 0);
+            final Integer n3 = Convert.toInt(split[2], 0);
+            final Integer n4 = Convert.toInt(split[3], 0);
+            top = n1;
+            bottom = n2;
+            left = n3;
+            right = n4;
+        }
+        return Border.builder()
+                .top(top)
+                .left(left)
+                .right(right)
+                .bottom(bottom)
+                .build();
+
+    }
+
 
 }

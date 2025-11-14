8bc6d7bc6a9c7888f84ab8615602a373ecdff468
mengfly <7957086+mengfly_p@user.noreply.gitee.com>
2025-11-14 21:38:10 +0800

    test(util): 调整测试断言参数顺序
    - 修改了 assertArrayEquals 方法中期望值与实际值的参数位置
    - 确保测试断言符合标准的参数顺序规范
    - 提高测试代码可读性和维护性

--- src/test/java/io/github/mengfly/excel/report/util/StyleUtilTest.java
+++ src/test/java/io/github/mengfly/excel/report/util/StyleUtilTest.java
@@ -9,19 +9,19 @@ public class StyleUtilTest {
     @Test
     public void testAnalysisStyle() {
         String style = "tableDataStyle {dataFormat:0 岁}";
-        Assert.assertArrayEquals(StyleUtil.analysisStyle(style).toArray(),
-                new String[]{"tableDataStyle", "{dataFormat:0 岁}"});
+        Assert.assertArrayEquals(new String[]{"tableDataStyle", "{dataFormat:0 岁}"},
+                StyleUtil.analysisStyle(style).toArray());
 
         style = "tableDataStyle1 tableDataStyle2";
-        Assert.assertArrayEquals(StyleUtil.analysisStyle(style).toArray(),
-                new String[]{"tableDataStyle1", "tableDataStyle2"});
+        Assert.assertArrayEquals(new String[]{"tableDataStyle1", "tableDataStyle2"},
+                StyleUtil.analysisStyle(style).toArray());
 
         style = "{dataFormat:0 岁} tableDataStyle2 {dataFormat:0 岁}";
-        Assert.assertArrayEquals(StyleUtil.analysisStyle(style).toArray(),
-                new String[]{"{dataFormat:0 岁}", "tableDataStyle2", "{dataFormat:0 岁}"});
+        Assert.assertArrayEquals(new String[]{"{dataFormat:0 岁}", "tableDataStyle2", "{dataFormat:0 岁}"},
+                StyleUtil.analysisStyle(style).toArray());
 
         style = "{dataFormat:0 岁} {dataFormat: ${stress.count} 岁}";
-        Assert.assertArrayEquals(StyleUtil.analysisStyle(style).toArray(),
-                new String[]{"{dataFormat:0 岁}", "{dataFormat: ${stress.count} 岁}"});
+        Assert.assertArrayEquals(new String[]{"{dataFormat:0 岁}", "{dataFormat: ${stress.count} 岁}"},
+                StyleUtil.analysisStyle(style).toArray());
     }
 }

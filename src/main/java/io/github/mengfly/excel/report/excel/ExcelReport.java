a3a8c7bd9c29c077cc68a9e53652c8d89fae300d
mengfly <7957086+mengfly_p@user.noreply.gitee.com>
2026-09-23 17:31:28 +0800

    feat(doc): 更新文档

--- src/main/java/io/github/mengfly/excel/report/excel/ExcelReport.java
+++ src/main/java/io/github/mengfly/excel/report/excel/ExcelReport.java
@@ -76,6 +76,7 @@ public class ExcelReport {
      * @param template 模板
      * @param name     sheet 页面名称， 如果名称重复则在名称后面自动添加序号
      * @param context  模板数据
+     * @return 导出的组件
      */
     public Container exportTemplate(ReportTemplate template, String name, DataContext context) {
         Container container = template.render(context);
